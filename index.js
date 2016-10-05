1de99fdfde02e2000fd3ed13aaeff24bc90bcfc9
Casey Olson <casey@bookcasey.com>
2016-10-05 18:47:47 -0500

    switch 0

--- index.js
+++ index.js
@@ -47,7 +47,8 @@ LockitronAccessory.prototype.setState = function (state, callback) {
     if (state == Characteristic.LockCurrentState.UNSECURED) {
       console.log('trying to undo what we just did...')
       console.log(this.service.getCharacteristic(Characteristic.LockTargetState));
-          //.setValue(Characteristic.LockCurrentState.SECURED);
+      this.service.getCharacteristic(Characteristic.LockTargetState).setValue(Characteristic.LockCurrentState.SECURED);
+      console.log(this.service.getCharacteristic(Characteristic.LockTargetState));
         }
 }
 
