ea129f42267fbca2c5c40c844270107b673011c2
Casey Olson <casey@bookcasey.com>
2016-10-05 10:35:51 -0500

    remove broken thing

--- index.js
+++ index.js
@@ -20,7 +20,6 @@ function LockitronAccessory(log, config) {
   this.service
     .getCharacteristic(Characteristic.LockTargetState)
     .on('get', this.getState.bind(this))
-    .on('set', this.setState.bind(this))
     .on('set', this.setState.bind(this));
 }
 
