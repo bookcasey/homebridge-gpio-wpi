0a4badcefbc653ab3390f572ba082006bb99a556
Casey Olson <casey@bookcasey.com>
2016-10-05 20:08:38 -0500

    david

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var Service, Characteristic;
 var storage = require('node-persist');
-
+// storage.init({dir:'persist'})
 module.exports = function(homebridge) {
   Service = homebridge.hap.Service;
   Characteristic = homebridge.hap.Characteristic;
@@ -10,12 +10,18 @@ function LockitronAccessory(log, config) {
   this.log = log;
   this.name = config["name"];
   this.service = new Service.LockMechanism(this.name);
-
+ // this.service
+ //   .getCharacteristic(Characteristic.LockCurrentState)
+ //   .on('get', this.getState.bind(this));
   this.service
     .getCharacteristic(Characteristic.LockTargetState)
+    //.on('get', this.getState.bind(this))
     .on('set', this.setState.bind(this));
 }
-
+//LockitronAccessory.prototype.getState = function(callback) {
+//    console.log('getting state called!');
+//    callback(null, 1);
+//}
 LockitronAccessory.prototype.setState = function (state, callback) {
     callback = callback || function() {};
     if (this.lockTimer) {
@@ -25,14 +31,12 @@ LockitronAccessory.prototype.setState = function (state, callback) {
 
     var currentState = (state == Characteristic.LockTargetState.SECURED) ?
         Characteristic.LockCurrentState.SECURED : Characteristic.LockCurrentState.UNSECURED;
-
     this.service
         .setCharacteristic(Characteristic.LockCurrentState, currentState);
     if (state == Characteristic.LockTargetState.UNSECURED) {
         this.lockTimer = setTimeout(
             function(caller) {
                 caller.setState(Characteristic.LockTargetState.SECURED);
-                caller.setState(Characteristic.LockCurrentState.SECURED);
                 console.log('timer over');
             },
             10000,
