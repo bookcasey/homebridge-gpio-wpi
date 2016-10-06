3fc19a9f8a47e0379134272d4b99d47b6983eb6f
Casey Olson <casey@bookcasey.com>
2016-10-05 19:25:00 -0500

    switch 0

--- index.js
+++ index.js
@@ -34,20 +34,20 @@ LockitronAccessory.prototype.getState = function(callback) {
 LockitronAccessory.prototype.setState = function (state, callback) {
     console.log('setState runs!');
 
-    // var currentState = (state == Characteristic.LockTargetState.SECURED) ?
-    //     Characteristic.LockCurrentState.SECURED : Characteristic.LockCurrentState.UNSECURED;
-    //
-    //   this.service
-    //     .setCharacteristic(Characteristic.LockCurrentState, currentState);
+    var currentState = (state == Characteristic.LockTargetState.SECURED) ?
+        Characteristic.LockCurrentState.SECURED : Characteristic.LockCurrentState.UNSECURED;
+
+      this.service
+        .setCharacteristic(Characteristic.LockCurrentState, currentState);
 
       callback();
 
     if (state == Characteristic.LockCurrentState.UNSECURED) {
       console.log('Trying to undo what we just did...');
 
-      this.service
-        .getCharacteristic(Characteristic.LockTargetState)
-        .setValue(Characteristic.LockCurrentState.SECURED);
+      // this.service
+      //   .getCharacteristic(Characteristic.LockTargetState)
+      //   .setValue(Characteristic.LockCurrentState.SECURED);
 
     }
 }
