78f1fac18ba67a6ca9db9d8f37c4a5cbf78f7d7f
Casey Olson <casey@bookcasey.com>
2016-10-05 19:48:46 -0500

    david

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var Service, Characteristic;
-var storage = require('node-persist');
-// storage.init({dir:'persist'})
+
 module.exports = function(homebridge) {
   Service = homebridge.hap.Service;
   Characteristic = homebridge.hap.Characteristic;
@@ -10,18 +9,11 @@ function LockitronAccessory(log, config) {
   this.log = log;
   this.name = config["name"];
   this.service = new Service.LockMechanism(this.name);
- // this.service
- //   .getCharacteristic(Characteristic.LockCurrentState)
- //   .on('get', this.getState.bind(this));
   this.service
     .getCharacteristic(Characteristic.LockTargetState)
-    //.on('get', this.getState.bind(this))
     .on('set', this.setState.bind(this));
 }
-//LockitronAccessory.prototype.getState = function(callback) {
-//    console.log('getting state called!');
-//    callback(null, 1);
-//}
+
 LockitronAccessory.prototype.setState = function (state, callback) {
     callback = callback || function() {};
     if (this.lockTimer) {
